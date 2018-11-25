19076ac88dad70cd84b196e11ef21affd1de73d3
Yener Tüz <yenertuz@Yeners-iMac.local>
2018-11-24 22:15:43 -0800

    saturday night

--- ps_functions.py
+++ ps_functions.py
@@ -1,5 +1,7 @@
 #!/usr/local/bin/python3.7
 
+import statistics
+
 def push_stack(stack_a, stack_b):
 	stack_a.insert(0, stack_b.pop(0))
 
@@ -98,4 +100,88 @@ def get_info(stack):
 	info = dict()
 	info["min"] = min(stack)
 	info["index"] = stack.index(info["min"])
-	return info	
+	return info
+
+def push_below_median_to_stack_b(stack_a, stack_b, ops_list, median):
+	while stack_a[0] < median:
+		ops_list.append("pb")
+		run_command("pb", stack_a, stack_b)
+	pivot = stack_a[0]
+	ops_list.append("ra")
+	run_command("ra", stack_a, stack_b)
+	while stack_a[0] != pivot:
+		if stack_a[0] < median:
+			ops_list.append("pb")
+			run_command("pb", stack_a, stack_b)
+		else:
+			ops_list.append("ra")
+			run_command("ra", stack_a, stack_b)
+
+def push_above_median_to_stack_b(stack_a, stack_b, ops_list, median):
+	while stack_a[0] >= median:
+		ops_list.append("pb")
+		run_command("pb", stack_a, stack_b)
+	pivot = stack_a[0]
+	ops_list.append("ra")
+	run_command("ra", stack_a, stack_b)
+	while stack_a[0] != pivot:
+		if stack_a[0] >= median:
+			ops_list.append("pb")
+			run_command("pb", stack_a, stack_b)
+		else:
+			ops_list.append("ra")
+			run_command("ra", stack_a, stack_b)
+
+def count_ops(index, count):
+	front = index
+	back = count - index
+	return min(front, back)
+
+def find_highest_and_push_to_a(stack_a, stack_b, ops_list):
+	mini_index = stack_b.index(min(stack_b))
+	maxi_index = stack_b.index(max(stack_b))
+	mini_ops = count_ops(mini_index, len(stack_b))
+	maxi_ops = count_ops(maxi_index, len(stack_b))
+	if mini_ops < maxi_ops:
+		is_mini = 1
+		target_index = mini_index
+	else:
+		is_mini = 0
+		target_index = maxi_index
+	target = stack_b[target_index]
+	if target_index < len(stack_b) / 2:
+		while stack_b[0] != target:
+			run_command("rb", stack_a, stack_b)
+			ops_list.append("rb")
+	else:
+		while stack_b[0] != target:
+			run_command("rrb", stack_a, stack_b)
+			ops_list.append("rrb")
+	ops_list.append("pa")
+	run_command("pa", stack_a, stack_b)
+	if is_mini == 1:
+		run_command("ra", stack_a, stack_b)
+		ops_list.append("ra")
+	
+	
+
+def push_from_stack_b_to_a(stack_a, stack_b, ops_list):
+	while len(stack_b) > 1:
+		find_highest_and_push_to_a(stack_a, stack_b, ops_list)
+	run_command("pa", stack_a, stack_b)
+	ops_list.append("pa")
+
+def sort_multiple(stack_a, stack_b, ops_list):
+	median = statistics.median(stack_a)
+	push_above_median_to_stack_b(stack_a, stack_b, ops_list, median)
+	push_from_stack_b_to_a(stack_a, stack_b, ops_list)
+	if is_sorted(stack_a) == True:
+		return
+	push_below_median_to_stack_b(stack_a, stack_b, ops_list, median)
+	while is_sorted(stack_a) == False:
+		ops_list.append("rra")
+		run_command("rra", stack_a, stack_b)
+	push_from_stack_b_to_a(stack_a, stack_b, ops_list)
+	while is_sorted(stack_a) == False:
+		ops_list.append("rra")
+		run_command("rra", stack_a, stack_b)
--- push_swap.py
+++ push_swap.py
@@ -2,6 +2,14 @@
 
 import os
 import ps_functions as ps
+import signal
+
+
+def signal_handler(signal, frame):
+	print(ops_list)
+	exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
 
 try:
 	f = open("numbers")
@@ -35,13 +43,17 @@ if count == 3:
 	ps.sort_three(stack_a, ops_list)
 elif count == 2:
 	ps.sort_two(stack_a, ops_list)
+else:
+	ps.sort_multiple(stack_a, stack_b, ops_list)
+'''
 elif ps.is_sorted(stack_a) == False:
-	while len(stack_a) > 3:
+	while len(stack_a) > 3 and ps.is_sorted(stack_a) == False:
 		ps.get_min_and_pb(stack_a, stack_b, ops_list)
 	ps.sort_three(stack_a, ops_list)
 	while len(stack_b) > 0:
 		stack_b.pop(0)
 		ops_list.append("pa")
+'''
 
 os.system("echo " + " ".join(ops_list) + " > ops")
 
